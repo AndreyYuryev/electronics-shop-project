2313add9381a3a259aaa746deba7e8ffbd98c8dc
Andrey <yuryev@yandex.ru>
2023-10-28 00:34:59 +0300

    change main

--- homework-6/main.py
+++ homework-6/main.py
@@ -2,7 +2,7 @@ from src.item import Item, InstantiateCSVError
 
 if __name__ == '__main__':
     # Файл items.csv отсутствует.
-    Item.instantiate_from_csv()
+    Item.instantiate_from_csv('itm.csv')
     # FileNotFoundError: Отсутствует файл item.csv
 
     # В файле items.csv удалена последняя колонка.
